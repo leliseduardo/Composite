e31e3c4219385ecf95bf397c0f17461912efb793
Eduardo Lelis <eduardo.lelis.co@gmail.com>
2024-07-07 15:42:59 -0300

    init

---
+++ src/test/java/org/example/composite/CompositeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.composite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompositeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
